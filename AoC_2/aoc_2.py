e5812043ac2126b1fe4bd6b371536376bb2a4032
Rihards <rihards@rihards.lv>
2023-12-04 22:25:31 +0200

    advent of code 2023 #2 part

--- AoC_2/aoc_2.py
+++ AoC_2/aoc_2.py
@@ -3,36 +3,40 @@ import re
 
 infile = sys.path[0] + "\input.txt"
 
-bag = {
-    'red'   : '12',
-    'green' : '13',
-    'blue'  : '14'
-
-}
-
 with open(infile, "r") as f:
     lines = [line.strip() for line in f.readlines()]
-    impossible_games = []
-    possible_games = []
+    min_cubes_for_game = []
 
     for line in lines:
         game = line.split(": ")[0]
         game_id = game.split(" ")[1]
-        game_results = line.split(": ")[1]
-
+        game_results = line.strip().split(": ")[1].replace("; ", ";").replace(", ", ",").replace(" ", "=")
+        cubes_needed = {
+            'red'   : '0',
+            'green' : '0',
+            'blue'  : '0'
+        }
         for game_result in game_results.split(";"):
-            this_game = dict(result.split(" ") for result in game_result.strip().split(", "))
-            possible_game = 1
+            game_result = game_result.strip()
+            this_game = list(result.split("=") for result in game_result.split(","))
+            this_game = dict([[result[1], result[0]] for result in this_game])
 
-            for count, cube in this_game.items():
-                if possible_game:
-                    for allowed_cube, allowed_count in bag.items():
-                        if cube == allowed_cube and int(count) > int(allowed_count):
-                            if game.split(" ")[1] not in impossible_games:
-                                impossible_games.append(game_id)
-                            possible_game = 0
-                            break
-        if game_id not in impossible_games:
-            possible_games.append(game_id)
+            for this_game_cube, this_game_count in sorted(this_game.items()):
+                for cube, count in sorted(cubes_needed.items()):
+                    if cube == this_game_cube and int(this_game_count) > int(count):
+                        cubes_needed[cube] = int(this_game_count)
 
-    print(sum(int(game) for game in possible_games))
+        if game not in min_cubes_for_game:
+            min_cubes_for_game.append([game, cubes_needed])
+
+    sum_of_power = 0
+
+    for game in min_cubes_for_game:
+        power = 1
+        for cubes in game:
+            if isinstance(cubes, dict):
+                for point in cubes.values():
+                    power = power * point
+                sum_of_power = sum_of_power + power
+
+    print(sum_of_power)
